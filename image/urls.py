cf8797ed5a7d91128734301be4981f2b68d386d5
mujeeb olagunju <hardeyshinnah.mo@gmail.com>
2024-06-20 06:20:55 -0700

    file route change

--- image/urls.py
+++ image/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import MediaUploadView
 
 urlpatterns = [
-    path('upload/', MediaUploadView.as_view(), name='media-upload'),
+    path('', MediaUploadView.as_view(), name='media-upload'),
 ]
--- mjblogserver/urls.py
+++ mjblogserver/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/blog/', include('blog.urls')),
     path('api/v1/user/', include('user.urls')),
-    path('api/v1/image/', include('image.urls'))
+    path('api/v1/file/', include('image.urls'))
 ]
